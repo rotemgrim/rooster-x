0c0cd5a16e3d3cf78b7f83ea6e4530648ba0c56b
Rotem Grimberg <rotemgrim@gmail.com>
2019-05-21 20:38:28 +0300

    broken image will appear as missing

---
+++ meta.ts
@@ -0,0 +1,24 @@
+//
+// interface Imeta {
+//     released: scrape; // const
+//     plot: scrape; // const
+//     awards: scrape; // const
+//     poster: scrape; // update monthly
+//
+//     title: imdb; // const
+//     year: imdb; // const
+//     runtime: imdb; // const
+//     genre: imdb; // const
+//     director: imdb; // const
+//     writer: imdb; // const
+//     actors: imdb; // const
+//     language: imdb; // const
+//     country: imdb; // const
+//     isAdult: imdb; // const
+//     ratings: imdb; // update daily
+//     votes: imdb; // update daily
+//     imdbId: imdb; // const
+//     type: imdb; // const
+//     totalSeasons: calculated; // update daily
+//     totalEpisodes: calculated; // update daily
+// }
